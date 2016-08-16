97aa64f545c373136e74b431566537cff5175d10
Drudi <mdrudi@gmail.com>
2016-08-16 12:16:07 -0300

    Versao inicial do form de criacao de Veiculos

--- frontend/javascript/app.js
+++ frontend/javascript/app.js
@@ -4,9 +4,28 @@
     app.controller('VehicleSearchController', ['$http', function($http){
         var results = this;
         results.vehicles = [];
-        $http.get('http://127.0.0.1:8000/vehicles').success(function(data){
-            results.vehicles = data;
-        });
+        //var searchFunction = function(params){
+            $http.get('http://127.0.0.1:8000/vehicles').success(function(data){
+                results.vehicles = data;
+            });
+        //}
+
+    } ]);
+
+    app.controller('VehiclesAddController', ['$http', function($http){
+        var addv = this;
+        addv.vehicle = {};
+
+        this.addVehicle = function(){
+            //alert(addv.vehicle.model);
+            $http.post('http://127.0.0.1:8000/vehicles', addv.vehicle)
+                .success(function(data){
+                    addv.vehicle = data;
+                })
+                .error(function(data){
+                    alert(JSON.stringify(data));
+                });
+        };
 
     } ]);
 
